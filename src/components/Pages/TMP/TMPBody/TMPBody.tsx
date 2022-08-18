2134a072b8ae4de6d50e2ecc53c1f73856f1cc43
Madjita <Xok-s@yandex.ru>
2022-08-18 11:54:49 +0700

    update
    update

--- src/components/Pages/TMP/TMPBody/TMPBody.tsx
+++ src/components/Pages/TMP/TMPBody/TMPBody.tsx
@@ -1,7 +1,11 @@
+import { height } from '@mui/system';
 import React from 'react';
 import TMPCardUser from './TMPCardUser/TMPCardUser';
 import TMPCardUser_v2 from './TMPCardUser_v2/TMPCardUser_v2';
+import './TMPBody.css'
 
+import label from "../../../../img/Ico.svg"
+import TMPLeftPanel from './TMPLeftPanel/TMPLeftPanel';
 
 type Props = {
     className?: string,
@@ -12,10 +16,11 @@ type Props = {
  
 const TMPBody:  React.FC<Props> = () => {
 
-
+    const[select,setSelect] = React.useState(0);
 
     return(
-        <div style={{background: '#d0d1d4', height:'calc(100vh - 60px)'}}>
+        <div style={{background: '#d0d1d4', height:'calc(100vh - 60px)',display: 'grid',gridTemplateColumns: '60px 1fr'}}>
+            <TMPLeftPanel/>
             <TMPCardUser_v2/>
         </div>
     )
---
+++ src/components/Pages/TMP/TMPBody/TMPLeftPanel/TMPLeftPanel.tsx
@@ -0,0 +1,37 @@
+import { height } from '@mui/system';
+import React from 'react';
+import './TMPLeftPanel.css'
+
+import label from "../../../../../img/Ico.svg"
+
+type Props = {
+    className?: string,
+}
+
+ 
+const TMPLeftPanel:  React.FC<Props> = () => {
+
+    const[select,setSelect] = React.useState(0);
+
+    return(
+        <div style={{background: '#333333'}}>
+                <div id={select === 0? 'select':'' } 
+                     className='leftPanel center' 
+                     onClick={e=>{setSelect(0)}}
+                >
+                <img
+                    style={select != 0 ? {filter:'invert(100%) sepia(100%) saturate(100%) hue-rotate(86deg) brightness(118%) contrast(1%)'}:{}}
+                    src={label}
+                    alt='NTS'
+                    loading="lazy"
+                />
+
+                </div>
+                <div id={select === 1? 'select':'' } className='leftPanel center' onClick={e=>{setSelect(1)}}>
+                    <text>Hello</text>
+                </div>
+        </div>
+    )
+}
+
+export default TMPLeftPanel;
